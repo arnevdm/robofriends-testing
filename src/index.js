ce5c981a87835be515ffa27dc6951e51669ca601
arnevdm <arnevanimus@gmail.com>
2022-04-09 17:16:48 +0200

    redux complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 //As long as we keep this at the top we can use import
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider, connect } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleWare from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
