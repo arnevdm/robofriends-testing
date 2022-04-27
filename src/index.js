014760883c4913d65e0c7e63201604ffdb7ab588
arnevdm <arnevanimus@gmail.com>
2022-04-27 18:01:51 +0200

    enzyme

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-//As long as we keep this at the top we can use import
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import thunkMiddleWare from "redux-thunk";
-import "./index.css";
-import App from "./containers/App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
+
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { requestRobots, searchRobots } from "./reducers";
+
+import "./index.css";
 
 const logger = createLogger();
 
-const rootReducer = combineReducers({ searchRobots, requestRobots });
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+
 const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleWare, logger)
+  rootReducers,
+  applyMiddleware(thunkMiddleware, logger)
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+registerServiceWorker();
