8d3e95240f73fc3850ae2403817c514b195a7b9d
arnevdm <arnevanimus@gmail.com>
2022-04-28 13:58:58 +0200

    testing

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
 import {
-  CHANGE_SEARCH_FIELD,
+  CHANGE_SEARCHFIELD,
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED,
@@ -11,7 +11,7 @@ const initialStateSearch = {
 
 export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
-    case CHANGE_SEARCH_FIELD:
+    case CHANGE_SEARCHFIELD:
       return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
@@ -19,9 +19,8 @@ export const searchRobots = (state = initialStateSearch, action = {}) => {
 };
 
 const initialStateRobots = {
-  isPending: false,
   robots: [],
-  error: "",
+  isPending: true,
 };
 
 export const requestRobots = (state = initialStateRobots, action = {}) => {
@@ -34,10 +33,7 @@ export const requestRobots = (state = initialStateRobots, action = {}) => {
         isPending: false,
       });
     case REQUEST_ROBOTS_FAILED:
-      return Object.assign({}, state, {
-        error: action.payload,
-        isPending: false,
-      });
+      return Object.assign({}, state, { error: action.payload });
     default:
       return state;
   }
